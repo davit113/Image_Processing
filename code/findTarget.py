4d6f53194dabf9fc3e6d4a5099281139fe10afa1
Davit Skhiladze <dato.sxiladze.01@gmail.com>
2024-01-06 14:47:28 +0400

    working, yellow colored shapes detecting

--- code/findTarget.py
+++ code/findTarget.py
@@ -1,7 +1,7 @@
 """ 
 In HSV colorspace
-    yeloww hue=50~70 saturation=210~250
-    blue   hue=185~200 saturation=175~235
+    yeloww hue=50~70 saturation=210~250 // in cv2 hue is about = 25~33 and saturation= 200~250
+    blue   hue=185~200 saturation=175~235  //// in cv2 hue is about = 25~33 and saturation= 210~250
  """
 
 import numpy as np
@@ -10,6 +10,85 @@ import time
 import captureImage
 
 SCALEFACTOR = 1.0
+
+from matplotlib import pyplot as plt
+
+
+
+def cleanImage(img=0):
+    SCALEFACTOR = 1
+    # img = cv2.imread('code/tt.jpg', cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread('code/tt.jpg')
+    # img = cv2.resize(img, (0,0), fx=SCALEFACTOR, fy=SCALEFACTOR)
+    
+    # hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    # hue1,sat1,v = cv2.split(hsv)
+
+    # _, maskHue1 = cv2.threshold(hue1, 90, 100, cv2.THRESH_BINARY)
+    # _, maskSat1 = cv2.threshold(sat1, 200, 2010, cv2.THRESH_BINARY)
+    # finalImg1 = cv2.bitwise_and(maskSat1,maskHue1) #i thinck we should denoice before last 3 operation, and after that we should apply morphology tranform(oppening i think)
+
+    img = cv2.imread('code/new_test_img.jpg', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('code/new_test_img.jpg')
+    img = cv2.resize(img, (0,0), fx=SCALEFACTOR, fy=SCALEFACTOR)
+
+    
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    hue1,sat1,v = cv2.split(hsv)
+
+    """ double threshholding for yelow color """
+    _, maskHue1 = cv2.threshold(hue1, 25, 255, cv2.THRESH_BINARY)
+    _, maskHue2 = cv2.threshold(hue1, 33, 255, cv2.THRESH_BINARY_INV)
+    maskYelowHue= cv2.bitwise_and(maskHue1, maskHue2)
+
+    """ double threshholding for yelow saturation"""
+    _, maskSat1 = cv2.threshold(sat1, 200, 255, cv2.THRESH_BINARY)
+    _, maskSat2 = cv2.threshold(sat1, 245, 255, cv2.THRESH_BINARY_INV)
+    maskYelowSat = cv2.bitwise_and(maskSat1, maskSat2)
+    
+
+    final_yelow_mask_image = cv2.bitwise_and(maskYelowHue, maskYelowSat)
+
+    # finalImg1 = cv2.bitwise_and(maskSat1,maskHue1) #i thinck we should denoice before last 3 operation, and after that we should apply morphology tranform(oppening i think)
+
+
+    cv2.imshow('finalYelow', final_yelow_mask_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+    titles = ['image', 'mask', 'h1', 's1', 'v', 'maskH1', 'maskS1', 'finalImg']
+    # images = [img, hsv, hue1, sat1, v,maskHue1, maskSat1, finalImg1]
+
+    # cv2.imshow("shapes", hsv)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+
+    # _, mask = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY_INV)
+
+    # kernal = np.ones((5,5), np.uint8)
+
+    # dilation = cv2.dilate(mask, kernal, iterations=2)
+    # erosion = cv2.erode(mask, kernal, iterations=1)
+    # opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+    # closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+    # mg = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernal)
+    # th = cv2.morphologyEx(mask, cv2.MORPH_TOPHAT, kernal)
+
+    # titles = ['image', 'mask', 'dilation', 'erosion', 'opening', 'closing', 'mg', 'th']
+    # images = [img, mask, dilation, erosion, opening, closing, mg, th]
+
+    for i in range(8):
+        plt.subplot(2, 4, i+1), plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+
+    # plt.show()
+cleanImage()
 
 
 def findTarget():
@@ -46,3 +125,29 @@ def findTarget():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
         
+
+
+
+
+def testFoo(img=0):
+    img = cv2.imread('code/tt.jpg', cv2.IMREAD_GRAYSCALE)
+    _, mask = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY_INV)
+
+    kernal = np.ones((5,5), np.uint8)
+
+    dilation = cv2.dilate(mask, kernal, iterations=2)
+    erosion = cv2.erode(mask, kernal, iterations=1)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+    mg = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernal)
+    th = cv2.morphologyEx(mask, cv2.MORPH_TOPHAT, kernal)
+
+    titles = ['image', 'mask', 'dilation', 'erosion', 'opening', 'closing', 'mg', 'th']
+    images = [img, mask, dilation, erosion, opening, closing, mg, th]
+
+    for i in range(8):
+        plt.subplot(2, 4, i+1), plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+
+    plt.show()
